d00f6ac7842dc588964f9e7436865c7dd9bcffc2
LuisEduardo100 <luisedu.lepa@gmail.com>
2025-06-01 17:16:30 -0300

    feat: função de like e favorite na pagina de curso por id

--- src/pages/CourseDetailsPage.tsx
+++ src/pages/CourseDetailsPage.tsx
@@ -5,6 +5,7 @@ import { LoadingSpinner } from "../components/common/LoadingSpinner";
 import {
   favoriteCourse,
   getCourseDetails,
+  getCourseDetailsNoAuth,
   likeCourse,
   unfavoriteCourse,
   unlikeCourse,
@@ -21,11 +22,9 @@ export default function CourseDetailsPage() {
 
   const [isLiked, setIsLiked] = useState<boolean>(false);
   const [isFavorited, setIsFavorited] = useState<boolean>(false);
-  const [tokenExists, setTokenExists] = useState<boolean>(false);
+  const token = sessionStorage.getItem("token");
 
   useEffect(() => {
-    setTokenExists(!!sessionStorage.getItem("token"));
-
     if (!id) {
       setError("ID do curso não fornecido!");
       setLoading(false);
@@ -35,7 +34,18 @@ export default function CourseDetailsPage() {
     const fetchDetails = async () => {
       try {
         setLoading(true);
-        const data = await getCourseDetails(id);
+        if (token) {
+          const data = await getCourseDetails(id);
+          if (data) {
+            setCourse(data);
+            setIsLiked(data.liked);
+            setIsFavorited(data.favorited);
+          } else {
+            setError("Curso não encontrado.");
+          }
+          return;
+        }
+        const data = await getCourseDetailsNoAuth(id);
         if (data) {
           setCourse(data);
           setIsLiked(data.liked);
@@ -57,8 +67,10 @@ export default function CourseDetailsPage() {
     fetchDetails();
   }, [id]);
 
+  console.log(course);
+
   const handleLikeClick = async () => {
-    if (!tokenExists) {
+    if (!token) {
       alert("Você precisa estar logado para curtir um curso!");
       return;
     }
@@ -73,7 +85,7 @@ export default function CourseDetailsPage() {
   };
 
   const handleFavoriteClick = async () => {
-    if (!tokenExists) {
+    if (!token) {
       alert("Você precisa estar logado para favoritar um curso!");
       return;
     }
@@ -122,32 +134,32 @@ export default function CourseDetailsPage() {
           <p className="text-lg text-gray-700 mb-4">{course.synopsis}</p>
 
           <div className="flex items-center gap-4 mb-4">
-            {tokenExists && (
+            {token && (
               <>
                 <button
                   onClick={handleLikeClick}
-                  className={`px-4 py-2 rounded ${
+                  className={`bg-main-red text-white px-4 py-2 rounded ${
                     isLiked
-                      ? "bg-green-500 text-white"
+                      ? "bg-yellow-500 text-white"
                       : "bg-gray-300 text-gray-700 hover:bg-gray-400"
                   }`}
                 >
-                  {isLiked ? "&#10003; Curtido" : "Curtir"}
+                  {isLiked ? "Curtido" : "Curtir"}
                 </button>
 
                 <button
                   onClick={handleFavoriteClick}
-                  className={`px-4 py-2 rounded ${
+                  className={`bg-main-red text-white px-4 py-2 rounded ${
                     isFavorited
                       ? "bg-yellow-500 text-white"
                       : "bg-gray-300 text-gray-700 hover:bg-gray-400"
                   }`}
                 >
-                  {isFavorited ? "&#9733; Favorito" : "Favoritar"}
+                  {isFavorited ? "Favorito" : "Favoritar"}
                 </button>
               </>
             )}
-            {!tokenExists && (
+            {!token && (
               <p className="text-sm text-gray-600">
                 Faça login para curtir e favoritar este curso.
               </p>
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,11 +1,34 @@
-import type { AxiosError } from "axios";
 import { api } from "../api/axios";
 import type { CourseDetails } from "../types/Course";
 
 export async function getCourseDetails(
   id: string
 ): Promise<CourseDetails | null> {
-  const response = await api.get(`/courses/${id}`).catch((err) => {
+  const token = sessionStorage.getItem("token");
+
+  const response = await api
+    .get(`/courses/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .catch((err) => {
+      if (err.response.status === 404) {
+        return console.error("Curso não encontrado: ", err.response);
+      }
+      return err;
+    });
+
+  if (response.status === 200) {
+    return response.data;
+  }
+  return null;
+}
+
+export async function getCourseDetailsNoAuth(
+  id: string
+): Promise<CourseDetails | null> {
+  const response = await api.get(`/course/${id}`).catch((err) => {
     if (err.response.status === 404) {
       return console.error("Curso não encontrado: ", err.response);
     }
@@ -19,53 +42,72 @@ export async function getCourseDetails(
 }
 
 export async function likeCourse(courseId: string): Promise<boolean> {
+  const token = sessionStorage.getItem("token");
   try {
-    const response = await api.post(`/courses/${courseId}/like`);
-    return response.status === 200; // Retorna true se sucesso
-  } catch (error) {
-    console.error(
-      "Erro ao curtir o curso:",
-      (error as AxiosError).response?.data || error
+    const response = await api.post(
+      `/likes`,
+      { courseId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
+    return response.status === 200 || response.status === 201;
+  } catch (error) {
+    console.error("Erro ao curtir o curso:", error);
     return false;
   }
 }
 
 export async function unlikeCourse(courseId: string): Promise<boolean> {
+  const token = sessionStorage.getItem("token");
+
   try {
-    const response = await api.delete(`/courses/${courseId}/like`);
-    return response.status === 200;
+    const response = await api.delete(`/likes`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { courseId },
+    });
+    return response.status === 200 || response.status === 204;
   } catch (error) {
-    console.error(
-      "Erro ao descurtir o curso:",
-      (error as AxiosError).response?.data || error
-    );
+    console.error("Erro ao descurtir o curso: ", error);
     return false;
   }
 }
 
 export async function favoriteCourse(courseId: string): Promise<boolean> {
+  const token = sessionStorage.getItem("token");
   try {
-    const response = await api.post(`/courses/${courseId}/favorite`);
-    return response.status === 200;
-  } catch (error) {
-    console.error(
-      "Erro ao favoritar o curso:",
-      (error as AxiosError).response?.data || error
+    const response = await api.post(
+      `/favorites`,
+      { courseId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
+    return response.status === 200 || response.status === 201;
+  } catch (error) {
+    console.error("Erro ao favoritar o curso: ", error);
     return false;
   }
 }
 
 export async function unfavoriteCourse(courseId: string): Promise<boolean> {
+  const token = sessionStorage.getItem("token");
   try {
-    const response = await api.delete(`/courses/${courseId}/favorite`);
-    return response.status === 200;
+    const response = await api.delete(`/favorites`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { courseId },
+    });
+    return response.status === 200 || response.status === 204;
   } catch (error) {
-    console.error(
-      "Erro ao desfavoritar o curso:",
-      (error as AxiosError).response?.data || error
-    );
+    console.error("Erro ao desfavoritar o curso:", error);
     return false;
   }
 }
